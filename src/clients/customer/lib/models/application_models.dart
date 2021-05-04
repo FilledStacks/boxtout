fcddeebf59adb852224198677b3f216c1af36bf6
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-05-04 15:00:28 +0800

    Adds new data models relavant to `PlacesService`

--- src/clients/customer/lib/models/application_models.dart
+++ src/clients/customer/lib/models/application_models.dart
@@ -17,3 +17,50 @@ class User with _$User {
 
   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
 }
+
+@freezed
+class PlacesDetails with _$PlacesDetails {
+  factory PlacesDetails({
+    String? placeId,
+    String? streetNumber,
+    String? streetShort,
+    String? streetLong,
+    String? state,
+    String? zip,
+    String? city,
+    String? searchString,
+    double? lat,
+    double? lng,
+  }) = _PlacesDetails;
+
+  factory PlacesDetails.fromJson(Map<String, dynamic> json) =>
+      _$PlacesDetailsFromJson(json);
+
+  static Map<String, dynamic>? locationToJson(Location? data) => data?.toJson();
+}
+
+@freezed
+class PlacesAutoCompleteResult with _$PlacesAutoCompleteResult {
+  factory PlacesAutoCompleteResult({
+    String? placeId,
+    String? mainText,
+    String? description,
+    String? secondaryText,
+  }) = _PlacesAutoCompleteResult;
+
+  factory PlacesAutoCompleteResult.fromJson(Map<String, dynamic> json) =>
+      _$PlacesAutoCompleteResultFromJson(json);
+}
+
+@freezed
+class Location with _$Location {
+  factory Location({
+    required String id,
+    double? longitude,
+    double? latitude,
+    String? placeName,
+  }) = _Location;
+
+  factory Location.fromJson(Map<String, dynamic> json) =>
+      _$LocationFromJson(json);
+}

6896983faa1662f7df93932e2014d82cea89e780
Dane Mackier <dane.mackier@outlook.com>
2021-05-16 08:25:40 +0200

    Adds dot env loading

--- src/clients/customer/lib/main.dart
+++ src/clients/customer/lib/main.dart
@@ -4,8 +4,10 @@ import 'package:stacked_services/stacked_services.dart';
 
 import 'app/app.locator.dart';
 import 'app/app.router.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 
 Future main() async {
+  await DotEnv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   setupLocator();

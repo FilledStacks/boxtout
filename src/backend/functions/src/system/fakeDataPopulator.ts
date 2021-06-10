bc9bc7c9d7c8a7e3e3bffa9095b760def1aa9f71
FilledStacks <dane.mackier@outlook.com>
2021-06-10 10:40:30 +0200

    Completes the fake data generation for emulator

--- src/backend/functions/src/system/fakeDataPopulator.ts
+++ src/backend/functions/src/system/fakeDataPopulator.ts
@@ -1,4 +1,5 @@
 import { firestore } from 'firebase-admin';
+import * as faker from 'faker';
 
 
 // enable short hand for console.log()
@@ -26,17 +27,53 @@ export class FakeDataPopulator {
 
     if (!generateDocument.exists) {
       await this.createGenerateDocument();
-
-      this.generateMerchants();
+      await this.generateMerchants();
     }
   }
 
-  private generateMerchants() {
+  private async generateMerchants() {
     log('generateMerchants');
-    
-    this.firestoreDatabase.collection('merchants').add({
-      'name': 'I am first merchant'
-    });
+
+    for (let index = 0; index < 30; index++) {
+      let merchant = {
+        'name': faker.commerce.productName(),
+        'image': faker.image.imageUrl(640, 640, 'food'),
+        'categories': [
+          faker.commerce.department(),
+          faker.commerce.department()
+        ],
+        'rating': faker.datatype.float(2),
+        'numberOfRatings': faker.datatype.number(200),
+      };
+
+      let merchantId = await this.createMerchantDocument(merchant);
+      await this.generateMerchantsProducts(merchantId);
+    }
+  }
+
+  private async generateMerchantsProducts(merchatId: string) {
+    log(`generateMerchantsProducts merchatId:${merchatId}`);
+
+    for (let index = 0; index < 30; index++) {
+      let product = {
+        'name': faker.commerce.productName(),
+        'description': faker.lorem.paragraph(2),
+        'image': faker.image.imageUrl(640, 640, 'food'),
+        'category': faker.commerce.department(),
+        'price': faker.datatype.number(8999),
+      };
+
+      await this.createMerchantProduct(merchatId, product);
+    }
+  }
+
+  private async createMerchantProduct(merchantId: string, product: any) {
+    await this.firestoreDatabase.collection('merchants').doc(merchantId).collection('products').add(product);
+  }
+
+  private async createMerchantDocument(merchant: any): Promise<string> {
+    let documentReference = await this.firestoreDatabase.collection('merchants').add(merchant);
+    return documentReference.id;
   }
 
   private async createGenerateDocument(): Promise<void> {

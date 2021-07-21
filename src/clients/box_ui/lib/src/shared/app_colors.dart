32883205ccc8d0dfd40ea26fa1dde8f645b038ce
Ebrahim Soliman <esoliman@nbsventure.com>
2021-07-21 16:44:18 +0300

    remove some colors

--- src/clients/box_ui/lib/src/shared/app_colors.dart
+++ src/clients/box_ui/lib/src/shared/app_colors.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 
 const Color kcPrimaryColor = Color(0xff22A45D);
-const Color kcBlackColor = Color(0xff1B2720);
 const Color kcMediumGreyColor = Color(0xff868686);
 const Color kcLightGreyColor = Color(0xffe5e5e5);
 const Color kcVeryLightGreyColor = Color(0xfff2f2f2);
-const Color kcWhiteColor = Color(0xffffffff);
-const Color kcTransparentColor = Color(0x00000000);
 const Color kcRedColor = Color(0xfff44336);
 const Color kcOrangeColor = Color(0xffff9800);
